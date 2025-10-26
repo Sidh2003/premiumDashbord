dea8c342b52c1b0e2d9a9ae9ff00410997b4d0b6
Sidh2003 <siddheshsonavane22@gmail.com>
2025-10-26 16:45:31 +0530

    d

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -15,12 +15,16 @@ class DashboardController extends Controller
     public function index(Request $request): View
     {
         $systemRoles = getSystemRoles();
-        $users = User::whereHas("roles", function($q) use($systemRoles){$q->whereIn("name", $systemRoles)->where('name','!=','SuperAdmin');})->count();
+        $users = User::whereHas("roles", function ($q) use ($systemRoles) {
+            $q->whereIn("name", $systemRoles)->where('name', '!=', 'SuperAdmin');
+        })->count();
         $employees = Employee::count();
 
         $enquiries = Enquiry::count();
 
 
-        return view('Admin.Dashboard.index', compact('users','employees', 'enquiries'));
+        return view('Admin.Dashboard.index', compact('users', 'employees', 'enquiries'));
     }
+
+
 }
